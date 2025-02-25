4a868e3ff60dffd8d404f5815864a8bb88c65c7b
Mahan <khalili1388mahan@gmail.com>
2025-02-25 14:47:04 +0330

    Add comments for more info

--- addons/signal_lens/inspector/signal_lens_remote_node_inspector.gd
+++ addons/signal_lens/inspector/signal_lens_remote_node_inspector.gd
@@ -8,7 +8,11 @@ extends EditorInspectorPlugin
 signal node_selected(node_path: NodePath)
 
 func _can_handle(object: Object) -> bool:
+	# If the requested node is a section in the project settings, it will skip receiving its
+	# signals. This line use node type inspection, the "SectionedInspectorFilter" type is a
+	# built-in type in the editor for the project settings. (See #21)
 	if object.get_property_list()[0]["name"] == "SectionedInspectorFilter": return false
+	
 	return object.get('Node/path') != null
 
 
